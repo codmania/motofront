531ec437b790fb3d7606377b412b255345737b3a
golden <goldensoftdev@gmail.com>
2017-06-07 17:30:11 +0200

    finish play page

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -34,3 +34,24 @@ function closeSideNav() {
     document.getElementById("home").style.marginLeft= "0";
     // document.body.style.backgroundColor = "white";
 }
+
+function tableSearch() {
+    // Declare variables 
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("searchTable");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
