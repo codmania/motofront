06707a4aac6f386602ed18b5889931c6163055c5
golden <goldensoftdev@gmail.com>
2017-06-25 17:52:59 +0200

    update table search

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -62,7 +62,7 @@ function closeSideNav() {
     // document.body.style.backgroundColor = "white";
 }
 
-function tableSearch() {
+function tableSearch(index) {
     // Declare variables 
     var input, filter, table, tr, td, i;
     input = document.getElementById("searchInput");
@@ -72,7 +72,7 @@ function tableSearch() {
 
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[1];
+        td = tr[i].getElementsByTagName("td")[index];
         if (td) {
             if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
                 tr[i].style.display = "";
