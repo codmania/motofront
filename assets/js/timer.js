7c2d995fb546092262eb5b38f4798301b5284c72
golden <goldensoftdev@gmail.com>
2017-07-07 10:06:17 +0200

    add countdown timer

---
+++ assets/js/timer.js
@@ -0,0 +1,52 @@
+var countDownDate = new Date("Oct 5, 2017 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    if (seconds < 10) {
+        seconds = '0' + seconds;
+    }
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+    if (hours < 10) {
+        hours = '0' + hours;
+    }
+    if (days < 10) {
+        days = '0' + days;
+    }
+    if (days > 99) {
+        days = 99;
+    }
+
+    // Output the result in an element with id="demo"
+    var $timer = $('#countdownTimer');
+    var template = _.template($('#timer-template').html());
+
+    $timer.html("");
+
+    $timer.append(template({
+        days: days,
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds,
+    }));
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
